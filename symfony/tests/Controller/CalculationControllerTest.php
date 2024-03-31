0e9111ec6797b0bd5db19c222faf12431e68c5cc
n*t_w*r <trololo557@gmail.com>
2024-03-31 12:07:35 +0300

    finish tests again...

--- symfony/tests/Controller/CalculationControllerTest.php
+++ symfony/tests/Controller/CalculationControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class CalculationControllerTest extends WebTestCase
 {
@@ -25,5 +26,23 @@ class CalculationControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $response = json_decode($client->getResponse()->getContent(), 1);
         $this->assertEquals(['cost' => 9000.0], $response);;
+    }
+
+    public function testCalculationError(): void
+    {
+        $client = static::createClient([
+            'environment' => 'test',
+            'debug' => false,
+        ]);
+        $content = [
+            'baseCost' => 10000.0,
+            'startDate' => '2025-04-01'
+        ];
+        $client->request(
+            method: 'POST',
+            uri: '/travel/api1/calculation/calculate',
+            content: json_encode($content, 1)
+        );
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
     }
 }
